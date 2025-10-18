58b685b1414afd7b672e3a7f1081c3136cd1b2d2
Koni <najeeullahnoon786@gmail.com>
2025-10-18 08:11:51 +0500

    refactor: add-game-timer-animations

--- Assets/Scripts/UIScripts/GameOverUI.cs
+++ Assets/Scripts/UIScripts/GameOverUI.cs
@@ -10,14 +10,23 @@ public class GameOverUI : MonoBehaviour
     [SerializeField] Image circularFill;
     [SerializeField] TextMeshProUGUI deliveryText;
     [SerializeField] Button RestartButton;
+    private Animator anim;
+    private const string BONUSTRIGGER = "BonusTrigger";
     float totalGameplayTimer;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
         Hide();
         totalGameplayTimer = KitchenGameManager.Instance.GetGamePlayTimer();
         KitchenGameManager.Instance.OnGameOver += Instance_OnGameOver1;
+        DeliveryCounter.Instance.OnOrderSuccess += Instance_OnOrderSuccess;
         RestartButton.onClick.AddListener(() => { Loader.ReloadScene(); });
+    }
+
+    private void Instance_OnOrderSuccess(object sender, System.EventArgs e)
+    {
+        anim.SetTrigger(BONUSTRIGGER);
     }
 
     private void Instance_OnGameOver1(object sender, System.EventArgs e)
